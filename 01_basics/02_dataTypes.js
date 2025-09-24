4879fcd70d51999eafb8be667c51accdaef65530
Prasenjit-Sahoo18 <mukeshsahoo902@gmail.com>
2025-09-24 21:06:54 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)// we are using nodejs , not browser
+
+
+console.log(3+3)// code readability should be high 
+
+console.log("hitesh")// for printing something in console
+
+let state= null ;
+let state2 ;// undefined 
+let name = "Mukesh" // string literal
+let age = 22 // number literal
+let isApproved = true // boolean literal
+let firstName = undefined // undefined literal
+let selectedColor = null // null literal
+let isLoggedIn = false // boolean literal   
+/*primitive data types 
+// data types in js
+// number => 2 to power 53 
+// bigint 
+// string => '',"",``
+// boolean => true/false
+// null => standalone value
+// undefined => variable value not assigned
+// symbol => unique value
+*/
+
+//object 
+
+console.log(typeof "age");
+console.log(typeof "mukesh");
+console.log(typeof 22);
+console.log(typeof true);
+console.log(typeof undefined);// undefined 
+console.log(typeof null); // object 
+/* type of null --- is a object in java script */
