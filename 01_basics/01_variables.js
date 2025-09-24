b604c293c36ab669b88db2dce64d6e409dd222d9
Prasenjit-Sahoo18 <mukeshsahoo902@gmail.com>
2025-09-24 20:19:04 +0530

    Add variables example

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId= 144553
+let accountEmail= "hitesh@goole.com "
+var accountPassword="12345"
+accountCity="Delhi"
+let accountState;
+
+// accountId =2 // not allowed 
+
+accountEmail ="hc@hc.com"
+accountPassword="21212121"
+ accountCity="Mumbai" 
+console.log(accountId);
+/*
+prefer not to use var
+because of issue in block scope and functional  scope
+
+
+*/
+console .table ([accountId,accountEmail,accountPassword,accountCity,accountState]);
