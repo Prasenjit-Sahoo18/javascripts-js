db0d852cfb8e5f3fdfedc5875f8d1c8a73f4f4a3
Prasenjit-Sahoo18 <mukeshsahoo902@gmail.com>
2025-09-27 12:31:58 +0530

    commit

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+/*let score =  null
+
+console.log(typeof score); // number
+console.log(typeof (score )); // string
+
+
+let valueInNumber = Number(score) // convert string to number
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber);  */
+
+
+let score =  undefined
+console.log(typeof score); // number
+console.log(typeof (score )); // string
+
+
+let valueInNumber = Number(score) // convert string to number
+console.log(typeof valueInNumber); // number
+console.log(valueInNumber); 
